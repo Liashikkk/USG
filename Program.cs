332b99ccf215226ab86c2b4459511a2a6307c6f6
dasd <safdsfdsfdsf@ddasdasd.sd>
2024-04-28 22:27:15 +0200

    base

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UGG
+{
+    internal class Program
+    {
+        const int STD_OUTPUT_HANDLE = -11;
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetStdHandle(int handle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool SetConsoleDisplayMode(IntPtr ConsoleHandle, uint Flags, IntPtr NewScreenBufferDimensions);
+        public static void FullScreen()
+        {
+            var hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+            SetConsoleDisplayMode(hConsole, 1, IntPtr.Zero);
+            Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+        }
+        static void Main(string[] args)
+        {
+            /*System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.Play();
+            player.Dispose();
+            */
+
+            FullScreen();
+            Console.ReadLine();
+        }
+    }
+}
